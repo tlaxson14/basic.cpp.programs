5db5c3fd4426b224d0ba3186e5bc63f02dcd185f
Travis Laxson <TravisLaxson@traviss-mbp.attlocal.net>
2017-05-01 22:28:19 -0500

    syntax and comment fix

--- chapter3/celsToFahr.cpp
+++ chapter3/celsToFahr.cpp
@@ -1,25 +1,38 @@
-
-
+/*********************************************************************************
+ This program takes input in degrees Celsius and converts into degrees Fahrenheit.
+**********************************************************************************/
 
 #include <iostream>
 
-void getConversionCelsiusToFahrenheit()
-{
-    double celsius, fahrTemperature;
+//Function prototype
+double getCelsiusToFahrenheit(double degreeCelsius);
 
-    std::cout << "Enter a temperature in degrees Celsius: " << std::endl;
-
-    std::cin >> celsius;
-
-    //Formula to convert celsius to fahrenheit
-    fahrTemperature = (9.0/5.0) * celsius + 32;
-
-    std::cout << celsius << " degrees Celsius = " << fahrTemperature << " degrees Fahrenheit." << std::endl;
-}
 
 int main()
 {
-  getConversionCelsiusToFahrenheit();
+    double celsius;
 
-  return 0;
+    //Enter degrees Celsius to convert to Fahrenheit
+    std::cout << "Enter a temperature in degrees Celsius: " << std::endl;
+    std::cin >> celsius;
+
+    //Show conversion by calling getCelsiusToFahrenheit function
+    std::cout << celsius << " degrees Celsius = " <<  getCelsiusToFahrenheit(celsius) << " degrees Fahrenheit." << std::endl;
+
+    return 0;
+}
+
+/*************** getCelsiusToFahrenheit function ************************
+This function takes the input in degrees Celsius and applies a formula to
+convert and return the temperature into degrees Fahrenheit.
+*************************************************************************/
+
+double getCelsiusToFahrenheit(double degreeCelsius)
+{
+    double fahrTemperature;
+
+    //Convert celsius to fahrenheit
+    fahrTemperature = (9.0/5.0) * degreeCelsius + 32;
+
+    return fahrTemperature;
 }
